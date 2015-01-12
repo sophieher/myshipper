5ab7e5f062cfc7df1ed49a9dac9cdf47cc876638
Sophie Her <iamher@iamhermusic.com>
2015-01-12 13:29:40 -0600

    Quick and dirty label response. Need to get label served up.

--- rates/api.py
+++ rates/api.py
@@ -2,20 +2,23 @@ import requests
 
 import xml.etree.ElementTree as ET
 
+def validate_required_rates(params):
+    # These are required parameters
+    if not params.get('o_zip') or not params.get('d_zip') or not params.get('lbs'):
+        #TODO: raise an error here before going on 
+        pass
+
 def get_rates_from_usps(params):
     data = {'data': 'something'}
     KEY = '564ORDOR2223'
     package = '1ST'
     
-    # These are required parameters
-    if not params.get('o_zip') or not params.get('d_zip') or not params.get('lbs'):
-        #TODO: raise an error here before going on 
-        pass
+    # check that the required params are good
+    if not validate_required_rates(params):
+        pass #raise the error
         
     if params.get('mail_class'):
-        service = params.get('mail_class')
-    else:
-        service = 'ALL'
+        service = params.get('mail_class', 'ALL')
     
     if service == 'FIRST CLASS' or service == 'FIRST CLASS COMMERCIAL' or service == 'FIRST CLASS HFP COMMERCIAL':
         if params.get('fcm_type'):
@@ -29,20 +32,12 @@ def get_rates_from_usps(params):
     
     pounds = params.get('lbs')
     
-    if params.get('oz'):
-        oz = params.get('oz')
-    else:
-        oz = 0
+
+    oz = params.get('oz', 0)
     
-    if params.get('size'):
-        size = params.get('size')
-    else:
-        size = 'REGULAR'
+    size = params.get('size', 'REGULAR')
     
-    if params.get('machinable'):
-        machinable = params.get('machinable')
-    else:
-        machinable = 'true'
+    machinable = params.get('machinable', 'true')
 
     xml_string = """<RateV4Request USERID="%s"><Revision/>
         <Package ID="%s">
@@ -70,5 +65,53 @@ def get_rates_from_usps(params):
         rate_d['Rate'] = rate.text
         rate_d['Mail Class'] = mail_s.text
         rate_l.append(rate_d)
-        
-    return rate_l
+    rate_response = {'data': rate_l}
+    return rate_response
+    
+def get_label_from_usps(params):
+    '''
+    Grabbing the label
+    '''
+    label_xml = '''<?xml version="1.0" encoding="UTF-8" ?>
+    <DelivConfirmCertifyV4.0Request USERID="161SELF05974" PASSWORD="136AV76AZ752">
+      <Revision>2</Revision>
+      <ImageParameters />
+      <FromName>John Doe</FromName>
+      <FromFirm>USPS</FromFirm>
+      <FromAddress1>RM 2100</FromAddress1>
+      <FromAddress2>475 LEnfant Plaza SW</FromAddress2>
+      <FromCity>Washington</FromCity>
+      <FromState>DC</FromState>
+      <FromZip5>20260</FromZip5>
+      <FromZip4/>
+      <ToName>Janice Dickens</ToName>
+      <ToFirm>XYZ Corporation</ToFirm>
+      <ToAddress1>Ste 100</ToAddress1>
+      <ToAddress2>2 Massachusetts Ave NE</ToAddress2>
+      <ToCity>Washington</ToCity>
+      <ToState>DC</ToState>
+      <ToZip5>20212</ToZip5>
+      <ToZip4 />
+      <ToPOBoxFlag></ToPOBoxFlag>
+      <WeightInOunces>10</WeightInOunces>
+      <ServiceType>Priority</ServiceType>
+      <SeparateReceiptPage>False</SeparateReceiptPage>
+      <POZipCode>20770</POZipCode>
+      <ImageType>TIF</ImageType>
+      <AddressServiceRequested>False</AddressServiceRequested>
+      <HoldForManifest>N</HoldForManifest>
+      <Container>NONRECTANGULAR</Container>
+      <Size>LARGE</Size>
+      <Width>7</Width>
+      <Length>20.5</Length>
+      <Height>15</Height>
+      <Girth>60</Girth>
+     <ReturnCommitments>true</ReturnCommitments>
+    </DelivConfirmCertifyV4.0Request> 
+    '''
+    label_url = 'https://secure.shippingapis.com/ShippingAPI.dll?API=DelivConfirmCertifyV4&XML='+label_xml
+    label_response = requests.get(label_url)
+    root = ET.fromstring(label_response.content)
+    image = root.find('DeliveryConfirmationLabel').text
+    img_name = root.find('DeliveryConfirmationNumber').text + '.png'
+    return image
--- rates/views.py
+++ rates/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_GET
 import requests
-from api import get_rates_from_usps
+from api import get_rates_from_usps, get_label_from_usps
 
 
 # View for getting rates from USPS
@@ -10,4 +10,14 @@ from api import get_rates_from_usps
 def rates(request):
     response = get_rates_from_usps(request.GET)
     print request.GET['o_zip']
-    return JsonResponse(response, safe=False)
+    return JsonResponse(response)
+  
+  
+# View for getting a label from USPS
+@require_GET    
+def label(request):
+    response = get_label_from_usps(request.GET)
+    # image = response.decode('base64')
+    # print image
+    # return HttpResponse(image, content_type="image/png")
+    return HttpResponse(response)
