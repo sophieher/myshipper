cadc2d35f90611a06eb6a46ae25994d4bcc5f328
Sophie Her <iamher@iamhermusic.com>
2015-01-12 16:32:00 -0600

    Change label output to PDF. Add BadRequestError and 400 status response.

--- rates/api.py
+++ rates/api.py
@@ -1,63 +1,65 @@
 import requests
-
 import xml.etree.ElementTree as ET
+
+USPS_KEY = '161SELF05974'
+USPS_PASS = '136AV76AZ752'
+
+#Error to throw might need JSON response rather than simple string
+class BadRequestError(Exception):
+    def __init__(self, number, description):
+        self.number = number
+        self.description = description
+    def __str__(self):
+        return 'Error number: %s\n Description: %s' % (self.number, self.description)
+        
 
 def validate_required_rates(params):
     # These are required parameters
     if not params.get('o_zip') or not params.get('d_zip') or not params.get('lbs'):
-        #TODO: raise an error here before going on 
-        pass
+        raise BadRequestError(1, 'You are missing the required parameters')
 
 def get_rates_from_usps(params):
-    data = {'data': 'something'}
-    KEY = '564ORDOR2223'
     package = '1ST'
     
     # check that the required params are good
-    if not validate_required_rates(params):
-        pass #raise the error
+    validate_required_rates(params)
         
-    if params.get('mail_class'):
-        service = params.get('mail_class', 'ALL')
-    
-    if service == 'FIRST CLASS' or service == 'FIRST CLASS COMMERCIAL' or service == 'FIRST CLASS HFP COMMERCIAL':
-        if params.get('fcm_type'):
-            fcm = params.get('fcm_type')
-        else:
-            # TODO: raise an error for this; it's required if the mail classes are as above
-            pass
-    
+    service = params.get('mail_class', 'ALL')
+    fcm = params.get('fcm_type', '')
     o_zip = params.get('o_zip')
     d_zip = params.get('d_zip')
-    
-    pounds = params.get('lbs')
-    
-
+    pounds = params.get('lbs', 0)
+    container = params.get('container', '')
     oz = params.get('oz', 0)
-    
     size = params.get('size', 'REGULAR')
-    
     machinable = params.get('machinable', 'true')
+    
+    if fcm == '' and service == 'FIRST CLASS' \
+    or service == 'FIRST CLASS COMMERCIAL' \
+    or service == 'FIRST CLASS HFP COMMERCIAL':
+        raise BadRequestError(2, 'You must specify a first class mail type for First Class Service')
 
     xml_string = """<RateV4Request USERID="%s"><Revision/>
         <Package ID="%s">
             <Service>%s</Service>
+            <FirstClassMailType>%s</FirstClassMailType>
             <ZipOrigination>%s</ZipOrigination>
             <ZipDestination>%s</ZipDestination>
             <Pounds>%s</Pounds>
             <Ounces>%s</Ounces>
-            <Container/>
+            <Container>%s</Container>
             <Size>%s</Size>
             <Machinable>%s</Machinable>
         </Package>
-    </RateV4Request>""" % (KEY, package, service, o_zip, d_zip, pounds, oz, size, machinable)
+    </RateV4Request>""" % \
+    (USPS_KEY, package, service, fcm, o_zip, d_zip, pounds, oz, container, size, machinable)
     
     url = 'http://production.shippingapis.com/ShippingAPI.dll?API=RateV4&XML='+xml_string
     
     response = requests.get(url)
     root = ET.fromstring(response.content)
-    print 'RESPONSE CONTENT ', response.content
-    rate_l = []
+        
+    rate_list = []
     for postage in root[0].findall('Postage'):
         rate_d = {}
         rate = postage.find('Rate')
@@ -65,53 +67,99 @@ def get_rates_from_usps(params):
         rate_d['Rate'] = rate.text
         rate_d['Mail Class'] = mail_s.text
         rate_l.append(rate_d)
-    rate_response = {'data': rate_l}
+    rate_response = {'data': rate_list}
     return rate_response
     
+    
+import itertools
+# Get helper for view for label retrieval    
 def get_label_from_usps(params):
     '''
     Grabbing the label
     '''
+    delivery_confirm = [USPS_KEY, USPS_PASS]
+    delivery_confirm.append(params.get('f_name', ''))
+    delivery_confirm.append(params.get('f_company', ''))
+    delivery_confirm.append(params.get('f_address_1'))
+    delivery_confirm.append(params.get('f_address_2',''))
+    delivery_confirm.append(params.get('f_city'))
+    delivery_confirm.append(params.get('f_state'))
+    delivery_confirm.append(params.get('f_zip5'))
+    delivery_confirm.append(params.get('f_zip4', ''))
+    
+    delivery_confirm.append(params.get('to_name', ''))
+    delivery_confirm.append(params.get('to_company', ''))
+    delivery_confirm.append(params.get('to_address_1'))
+    delivery_confirm.append(params.get('to_address_2',''))
+    delivery_confirm.append(params.get('to_city'))
+    delivery_confirm.append(params.get('to_state'))
+    delivery_confirm.append(params.get('to_zip5'))
+    delivery_confirm.append(params.get('to_zip4', ''))
+    delivery_confirm.append(params.get('to_pob', ''))
+    
+    delivery_confirm.append(params.get('weight', ''))
+    delivery_confirm.append(params.get('mail_class', ''))
+    delivery_confirm.append(params.get('separate_receipt', ''))
+    delivery_confirm.append(params.get('po_zip', ''))
+    delivery_confirm.append(params.get('address_service_request',''))
+    delivery_confirm.append(params.get('hold_for_manifest',''))
+    
+    delivery_confirm.append(params.get('container', ''))
+    delivery_confirm.append(params.get('size',''))
+    delivery_confirm.append(params.get('width', ''))
+    delivery_confirm.append(params.get('length', ''))
+    delivery_confirm.append(params.get('height', ''))
+    delivery_confirm.append(params.get('girth', ''))
+    
+    delivery_confirm.append(params.get('return_commitments',''))
+    
     label_xml = '''<?xml version="1.0" encoding="UTF-8" ?>
-    <DelivConfirmCertifyV4.0Request USERID="161SELF05974" PASSWORD="136AV76AZ752">
+    <DelivConfirmCertifyV4.0Request USERID="%s" PASSWORD="%s">
       <Revision>2</Revision>
       <ImageParameters />
-      <FromName>John Doe</FromName>
-      <FromFirm>USPS</FromFirm>
-      <FromAddress1>RM 2100</FromAddress1>
-      <FromAddress2>475 LEnfant Plaza SW</FromAddress2>
-      <FromCity>Washington</FromCity>
-      <FromState>DC</FromState>
-      <FromZip5>20260</FromZip5>
-      <FromZip4/>
-      <ToName>Janice Dickens</ToName>
-      <ToFirm>XYZ Corporation</ToFirm>
-      <ToAddress1>Ste 100</ToAddress1>
-      <ToAddress2>2 Massachusetts Ave NE</ToAddress2>
-      <ToCity>Washington</ToCity>
-      <ToState>DC</ToState>
-      <ToZip5>20212</ToZip5>
-      <ToZip4 />
-      <ToPOBoxFlag></ToPOBoxFlag>
-      <WeightInOunces>10</WeightInOunces>
-      <ServiceType>Priority</ServiceType>
-      <SeparateReceiptPage>False</SeparateReceiptPage>
-      <POZipCode>20770</POZipCode>
-      <ImageType>TIF</ImageType>
-      <AddressServiceRequested>False</AddressServiceRequested>
-      <HoldForManifest>N</HoldForManifest>
-      <Container>NONRECTANGULAR</Container>
-      <Size>LARGE</Size>
-      <Width>7</Width>
-      <Length>20.5</Length>
-      <Height>15</Height>
-      <Girth>60</Girth>
-     <ReturnCommitments>true</ReturnCommitments>
+      
+      <FromName>%s</FromName>
+      <FromFirm>%s</FromFirm>
+      <FromAddress1>%s</FromAddress1>
+      <FromAddress2>%s</FromAddress2>
+      <FromCity>%s</FromCity>
+      <FromState>%s</FromState>
+      <FromZip5>%s</FromZip5>
+      <FromZip4>%s</FromZip4>
+      
+      <ToName>%s</ToName>
+      <ToFirm>%s</ToFirm>
+      <ToAddress1>%s</ToAddress1>
+      <ToAddress2>%s</ToAddress2>
+      <ToCity>%s</ToCity>
+      <ToState>%s</ToState>
+      <ToZip5>%s</ToZip5>
+      <ToZip4>%s</ToZip4>
+      
+      <ToPOBoxFlag>%s</ToPOBoxFlag>
+      <WeightInOunces>%s</WeightInOunces>
+      <ServiceType>%s</ServiceType>
+      <SeparateReceiptPage>%s</SeparateReceiptPage>
+      <POZipCode>%s</POZipCode>
+      <ImageType>PDF</ImageType>
+      <AddressServiceRequested>%s</AddressServiceRequested>
+      <HoldForManifest>%s</HoldForManifest>
+      
+      <Container>%s</Container>
+      <Size>%s</Size>
+      <Width>%s</Width>
+      <Length>%s</Length>
+      <Height>%s</Height>
+      <Girth>%s</Girth>
+      
+     <ReturnCommitments>%s</ReturnCommitments>
     </DelivConfirmCertifyV4.0Request> 
-    '''
+    ''' % (tuple(delivery_confirm))
     label_url = 'https://secure.shippingapis.com/ShippingAPI.dll?API=DelivConfirmCertifyV4&XML='+label_xml
     label_response = requests.get(label_url)
     root = ET.fromstring(label_response.content)
+    if root.tag == 'Error':
+        raise BadRequestError(4, root.find('Description').text)
     image = root.find('DeliveryConfirmationLabel').text
     img_name = root.find('DeliveryConfirmationNumber').text + '.png'
     return image
--- rates/views.py
+++ rates/views.py
@@ -2,22 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_GET
 import requests
-from api import get_rates_from_usps, get_label_from_usps
+from api import get_rates_from_usps, get_label_from_usps, BadRequestError
 
 
+def bad_error(e):
+    data = HttpResponse(status=400)  
+    data.write(e)    
+    return data
+    
 # View for getting rates from USPS
 @require_GET
 def rates(request):
-    response = get_rates_from_usps(request.GET)
-    print request.GET['o_zip']
-    return JsonResponse(response)
+    try:
+        response = get_rates_from_usps(request.GET)
+        return JsonResponse(response)
+    except BadRequestError as e:        
+        return bad_error(e)
   
   
 # View for getting a label from USPS
 @require_GET    
 def label(request):
-    response = get_label_from_usps(request.GET)
-    # image = response.decode('base64')
-    # print image
-    # return HttpResponse(image, content_type="image/png")
-    return HttpResponse(response)
+    try:
+        response = get_label_from_usps(request.GET)
+        image = response.decode('base64')
+        data = HttpResponse(image, content_type="application/pdf")
+        # data['Content-Disposition'] = 'attachment; filename=myShipperLabel.png'
+        return data
+        # return HttpResponse(response)
+    except BadRequestError as e:  
+        data = HttpResponse(status=400)  
+        data.write(e)    
+        return bad_error(e)
